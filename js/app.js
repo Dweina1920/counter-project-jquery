e255b5ba871b4f9a3f3d186e2877a031417efbd5
Dina <dina.yamanid@gmail.com>
2023-05-09 11:12:01 +0200

    contador y estilos terminados

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
-let counter = 0;
+
 
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+        let counter = 0;
+        
+        let butNext = $('.nextBtn')
+        let butPrev = $('.prevBtn')
+        butNext.click(function(){
+            counter++
+            $('#counter').text(counter)
+            console.log('hola', counter)
+            if (counter>0){
+                $('#counter').addClass('positivo')
+            } else if(counter <0){
+                $('#counter').addClass('negativo')
+            } else{
+                $('#counter').removeClass('positivo negativo')
+            }
+        })
+        butPrev.click(function(){
+            counter--
+            $('#counter').text(counter)
+            console.log('hola', counter)
+            if (counter>0){
+                $('#counter').addClass('positivo')
+            } else if(counter <0){
+                $('#counter').addClass('negativo')
+            } else{
+                $('#counter').removeClass('positivo negativo')
+            }
+        })
+
 });
+
